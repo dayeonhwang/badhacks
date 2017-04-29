d30c928f80149aa1508e1c289ccba8ce5e2813df
dayeonhwang <dahwang2018@u.northwestern.edu>
2017-04-29 17:38:13 -0500

    updated redirection links

--- content.js
+++ content.js
@@ -1,34 +1,34 @@
 $.confirm({
-  title: "Hi folks",
+  title: "Hi folks, what I mean by game design is",
   content: '<img src="http://www.mccormick.northwestern.edu/magazine/images/spring-2016/computer-science-horswill.jpg">',
   animationClose: 'top',
     buttons: {
         info: {
-            btnClass: 'btn-blue',
+            btnClass: 'btn-purple',
             action: function(){
-              window.location="https://www.linkedin.com/in/ian-horswill-46a52413/"
+              window.location="http://www.cs.northwestern.edu/~ian/"
             }
         },
         danger: {
             btnClass: 'btn-red any-other-class',
             action: function(){
-              window.location="https://twitter.com/ianhorswill?lang=en"
+              window.location="https://vimeo.com/78173848"
             }
         },
         warning: {
             btnClass: 'btn-warning',
             action: function(){
-              window.location="http://www.cs.northwestern.edu/~ian/"
+              window.location="https://www.youtube.com/watch?v=ZLF5dz9SM0w"
             }
         },
         piazza: {
-          btnClass: 'btn-warning',
+          btnClass: 'btn-blue',
           action: function() {
             window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           }
         },
         rickrolling: {
-          btnClass: 'btn-blue',
+          btnClass: 'btn-green',
           action: function() {}
         },
     },
