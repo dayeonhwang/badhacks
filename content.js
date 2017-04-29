4ee633c7bd27d2c67111d63bbb1c76c976d71617
shinandruth <lleeshin@gmail.com>
2017-04-29 18:19:33 -0500

    added meet

--- content.js
+++ content.js
@@ -32,9 +32,15 @@ $.confirm({
           action: function() {}
         },
         learn: {
-          btnClass: 'btn-purple',
+          btnClass: '.btn-lg',
           action: function() {
               window.location="https://docs.racket-lang.org/guide/intro.html#%28part._.Interacting_with_.Racket%29"
+          }
+        },
+        meet: {
+          btnClass: '.btn-success',
+          action: function() {
+              window.location="https://youtu.be/ZLF5dz9SM0w?t=70"
           }
         },
     },
