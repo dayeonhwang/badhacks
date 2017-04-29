917d60765438b6e06aa54d5e6ec2d866a339554f
dayeonhwang <dahwang2018@u.northwestern.edu>
2017-04-29 17:15:30 -0500

    updated redirection

--- content.js
+++ content.js
@@ -1,18 +1,35 @@
 $.confirm({
   title: "Hi folks",
   content: '<img src="http://www.mccormick.northwestern.edu/magazine/images/spring-2016/computer-science-horswill.jpg">',
-  //animation: 'zoom',
   animationClose: 'top',
     buttons: {
         info: {
             btnClass: 'btn-blue',
-            action: function(){}
+            action: function(){
+              window.location="https://www.linkedin.com/in/ian-horswill-46a52413/"
+            }
         },
         danger: {
-            btnClass: 'btn-red any-other-class', // multiple classes.
+            btnClass: 'btn-red any-other-class',
+            action: function(){
+              window.location="https://twitter.com/ianhorswill?lang=en"
+            }
         },
         warning: {
             btnClass: 'btn-warning',
+            action: function(){
+              window.location="http://www.cs.northwestern.edu/~ian/"
+            }
+        },
+        piazza: {
+          btnClass: 'btn-warning',
+          action: function() {
+            window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          }
+        },
+        rickrolling: {
+          btnClass: 'btn-blue',
+          action: function() {}
         },
     }
 });
